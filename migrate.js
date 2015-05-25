2d4ac1a757476f6b5138cb7856f3b4c5769b0186
Adam Malantonio <adam.malantonio@gmail.com>
2015-05-25 10:06:48 -0400

    renamed index / more opts than necessary

--- migrate.js
+++ migrate.js
@@ -10,7 +10,7 @@ var fs = require('fs')
   , argv = require('minimist')(process.argv.slice(2))
   ;
 
-var configPath = argv['config']) || argv['c'] || path.join(__dirname, 'config.json');
+var configPath = argv['config'] || path.join(__dirname, 'config.json');
 
 if ( !fs.statSync(configPath).isFile() ) return console.warn('No config.json file found');
 
@@ -18,16 +18,14 @@ var key = new WSKey(config.wskey.public, config.wskey.secret, {user: config.user
   , ncip = new NCIP(config.agencyID, key)
   , file = argv._[0]
   , parser = parse()
-  , errMode = !!(argv['err'] || argv['e'])
   , checkIn = !!(argv['check-in'])
   , checkOut = !!(argv['check-out'])
   , debug = !!argv['debug']
+  , errMode = !!(argv['e'] || argv['err'])
   , quiet = !!(argv['q'] || argv['quiet'])
   , time = argv['t'] || argv['time'] || 2500
   , record
   ;
-
-return console.log(path.join(__dirname, 'config.json'));
 
 if ( argv['h'] || argv['help'] ) {
   return showHelp();
@@ -44,7 +42,7 @@ if ( !checkIn && !checkOut && !debug) {
 fs.readFile(file, function(err, data) {
   if (err) throw err;
 
-  parse(data, {trim: true}, function(err, d) {
+  parse(data, { trim: true }, function(err, d) {
     if (err) throw err;
     var i = -1
       , len = d.length
@@ -98,5 +96,13 @@ fs.readFile(file, function(err, data) {
 function showHelp() {
   var name = path.basename(process.argv[1]);
   console.log('Batch check in/out items in WMS');
-  console.log('Usage: %s [opt] path/to/file.csv', name)
+  console.log('Usage: ./%s [opt] path/to/file.csv', name);
+  console.log('')
+  console.log('OPTIONS');
+  console.log('--check-in | --check-out | --debug   set mode');
+  console.log('--time=<interval> | -t <interval>    frequency of transaction in ms (default is 2500)');
+  console.log('--config=<path>                      set path to config.json file');
+  console.log('-q                                   quiet mode (no feedback)');
+  console.log('-e                                   only displays errors');
+  console.log('--help | -h                          display this menu');
 }
